b47006946a673eaa945d7afab1cc465becc85225
Trader <local>
2022-09-08 13:40:14 -0700

    Import current weeks lineup and move into df

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import os
+
+ROOT_DIR = os.path.dirname(__file__)
+thisWeeksFile = 'DKSalaries_09112022.csv'
+
+df = pd.read_csv(os.path.join(ROOT_DIR, thisWeeksFile))
+df[['team', 'gameTime']] = df['Game Info'].str.split('@', expand=True)
+df[['opp', 'date', 'time', 'timezone']] = df['gameTime'].str.split(' ', expand=True); 
+
+
+print(df)
