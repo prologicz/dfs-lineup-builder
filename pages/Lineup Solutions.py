3b5abf693733a362a479d28e45b37fdfc07fe967
Trader <local>
2022-09-19 15:27:07 -0700

    Create Download button to get DK ingest file

--- pages/Lineup Solutions.py
+++ pages/Lineup Solutions.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from st_aggrid import *
+from reports import draftKingsAllLineups, summaryReport
 
 st.title('Fantasy Football Optimizer')
 
@@ -7,7 +8,18 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-if 'summary_report' not in st.session_state:
+if 'solutions' not in st.session_state:
     st.info('File Ingest has not been completed')
 else:
-    AgGrid(st.session_state['summary_report'], fit_columns_on_grid_load = True)
+    summary_report = summaryReport(st.session_state['data'], st.session_state['solutions'])
+    AgGrid(summary_report, fit_columns_on_grid_load = True)
+
+    lineups = draftKingsAllLineups(st.session_state['data'], st.session_state['solutions'])
+    csv = lineups.to_csv(index=False)
+
+    st.sidebar.title('Data Download')
+    st.sidebar.download_button(
+        label = 'Download Draftkings File',
+        data = csv,
+        file_name='dk_all_lineups.csv'
+    )
